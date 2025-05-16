0fe0c44543ca322eb82e3e3f2443bd52ec41665f
Satendra Kumar Parteti <satendrakumarparteti.work@gmail.com>
2025-05-16 17:05:52 +0530

    Initial ReactFlare Starter

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="w-screen h-screen flex items-center flex-col justify-center text-center pb-2 text-3xl font-semibold tracking-tight">
+      <h1 className="text-3xl font-bold">ReactFlare is Ready!</h1>
+      <code className="relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold">
+        <a
+          href="https://github.com/satendra03"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          by @satendra_03
+        </a>
+      </code>
+    </div>
+  );
+}
+
+export default App;
