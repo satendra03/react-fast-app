94f83e14ddea9b05dac1e159aa2e7bb7794c008d
Satendra Kumar Parteti <satendrakumarparteti.work@gmail.com>
2025-05-18 21:45:22 +0530

    js and ts

---
+++ tailwind-shadcn-ts/src/App.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const App: React.FC = () => {
+  return (
+    <div className="w-screen h-screen flex items-center flex-col justify-center text-center pb-2 text-3xl font-semibold tracking-tight bg-black">
+      <h1 className="text-3xl font-bold text-white/50 mb-2 font-mono">
+        ReactFast Application is Ready!
+      </h1>
+      <code className="relative rounded bg-muted-foreground px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold">
+        <a
+          href="https://github.com/satendra03"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          by @satendra03
+        </a>
+      </code>
+    </div>
+  );
+};
+
+export default App;
